1069970c9d40e48e806e996e558b188ad35bd3bf
rgsax <riccardog.sax@gmail.com>
2018-07-02 22:43:01 +0200

    varie correzioni

---
+++ query_01.sql
@@ -0,0 +1,2 @@
+INSERT INTO NEGOZIO
+values(123456, 090337654, "ragione sociale", "via sede fisica 10", "via sede legale 11");
---
+++ query_02.sql
@@ -0,0 +1,4 @@
+UPDATE DIPENDENTE
+SET telefono = "090337642",
+	indirizzo_residenza = "via indirizzo residenza 20"
+WHERE matricola = 12346;
